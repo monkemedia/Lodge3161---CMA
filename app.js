dbb07f6808c0dd7128e4897996bbe593a65d66bd
monkemedia <info@monkemedia.co.uk>
2018-11-28 18:15:32 +0000

    Uploaded images are now deleted after 1 hour

--- app.js
+++ app.js
@@ -1,51 +1,61 @@
-const express = require('express');
-const app = express();
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const findRemoveSync = require('find-remove')
 
-const contentfulManagementRoutes = require('./api/routes/contentfulManagement');
-const authRoutes = require('./api/routes/auth.js');
+const contentfulManagementRoutes = require('./api/routes/contentfulManagement')
+const authRoutes = require('./api/routes/auth.js')
 
-const cors = require('cors');
+const cors = require('cors')
 
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({ extended: true }));
+const result = findRemoveSync('./public/uploads', {
+  age: {
+    seconds: 3600
+  },
+  files: '*.*'
+})
+
+result
+
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json({
   limit: '50mb', extended: true
-}));
-app.use(cors());
+}))
+app.use(cors())
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
+  )
 
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET')
+    return res.status(200).json({})
   }
-  next();
-});
+  next()
+})
 
 // Routes which should handle requests
-app.use('/api/v1/contentful-management', contentfulManagementRoutes);
-app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/contentful-management', contentfulManagementRoutes)
+app.use('/api/v1/auth', authRoutes)
 
 app.use((req, res, next) => {
-  const error = new Error('Not found');
-  error.status = 404;
-  next(error);
-});
+  const error = new Error('Not found')
+  error.status = 404
+  next(error)
+})
 
 app.use((error, req, res, next) => {
-  res.status(error.status || 500);
+  res.status(error.status || 500)
   res.json({
     error: {
       message: error.message
     }
-  });
-});
+  })
+})
 
-module.exports = app;
+module.exports = app
