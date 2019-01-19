87e9fe8313db2101b58453f008c26fb9532c14c3
monkemedia <info@monkemedia.co.uk>
2019-01-19 12:44:21 +0000

    Delete API checks to see if entries are published before unpublishing and then deleting

--- api/controllers/content/deletePage.js
+++ api/controllers/content/deletePage.js
@@ -10,66 +10,55 @@ exports.deletePage = (req, res, next) => {
     })
     .then(environment => {
       return environment.getEntry(entryId)
-      // return getAllEntryIds(environment, entryId)
-      //   .then(ids => {
-      //     return getEntries(ids, environment)
-      //       .then(entries => {
-      //         // Now get Child Entries
-      //         console.log('MOUSE', entries);
-      //         return getChildEntries(entries, environment)
-      //       })
-      //   })
-      //   .then(id => {
-      //     // Now unpublish page
-      //     return unpublishEntry(id)
-      //   })
-      //   .then(entry => {
-      //     // Now delete page
-      //     return entry.delete()
-      //   })
-      .then(entry => {
-        // We now have the page, lets get Child entries
-        const fields = entry.fields
-        const promises = [entry]
-
-        Object.keys(fields).forEach((key) => {
-          if (fields[key]['en-GB'].sys) {
-            promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
-          }
-        })
-
-        return Promise.all(promises)
-      })
-      .then(entries => {
-        // Now lets get Grand Child entries
-        const promises = entries
-        // console.log('PROMISES', promises);
-
-        entries.map(entry => {
+        .then(entry => {
+          // We now have the page, lets get Child entries
           const fields = entry.fields
+          const promises = [entry]
 
           Object.keys(fields).forEach((key) => {
             if (fields[key]['en-GB'].sys) {
-              if (fields[key]['en-GB'].sys.linkType === 'Asset') {
-                promises.push(environment.getAsset(fields[key]['en-GB'].sys.id))
-              } else {
-                promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
-              }
-            } 
+              promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
+            }
+          })
+
+          return Promise.all(promises)
+        })
+        .then(entries => {
+          // Now lets get Grand Child entries
+          const promises = entries
+
+          entries.map(entry => {
+            const fields = entry.fields
+
+            Object.keys(fields).forEach((key) => {
+              if (fields[key]['en-GB'].sys) {
+                if (fields[key]['en-GB'].sys.linkType === 'Asset') {
+                  promises.push(environment.getAsset(fields[key]['en-GB'].sys.id))
+                } else {
+                  promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
+                }
+              } 
+            })
+          })
+
+          return Promise.all(promises)
+        })
+        .then(allEntries => {
+          // Lets unpublish ALL entries of Page
+          return allEntries.map(entry => {
+            const publishedVersion = entry.sys.publishedVersion
+            const version = entry.sys.version
+            // lets see if its published
+            if (publishedVersion && ((publishedVersion + 1) >= version)) {
+              // Now unpublish it
+              return entry.unpublish()
+            }
+            return entry
           })
         })
-
-        return Promise.all(promises)
-      })
-      .then(allEntries => {
-        // Lets unpublish ALL entries of Page
-        allEntries.map(entry => {
-          return entry.unpublish()
-            .then(() => {
-              return entry.delete()
-            })
+        .then(() => {
+          return entry.delete()
         })
-      })
     })
     .catch(err => {
       res.status(500).send({ error: err })
