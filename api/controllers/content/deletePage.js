04d90bc8998d56dba2d59bed75ef43d410fb5ad6
monkemedia <info@monkemedia.co.uk>
2019-01-22 22:35:57 +0000

    Delete Page API has been created

--- api/controllers/content/deletePage.js
+++ api/controllers/content/deletePage.js
@@ -22,43 +22,111 @@ exports.deletePage = (req, res, next) => {
           })
 
           return Promise.all(promises)
-        })
-        .then(entries => {
-          // Now lets get Grand Child entries
-          const promises = entries
+            .then(entries => {
+              // Now lets get Grand Child entries
+              // const promises = entries
 
-          entries.map(entry => {
-            const fields = entry.fields
+              return entries.map(ent => {
+                const fields = ent.fields
 
-            Object.keys(fields).forEach((key) => {
-              if (fields[key]['en-GB'].sys) {
-                if (fields[key]['en-GB'].sys.linkType === 'Asset') {
-                  promises.push(environment.getAsset(fields[key]['en-GB'].sys.id))
-                } else {
-                  promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
-                }
-              } 
+                return Object.keys(fields).forEach((key) => {
+                  if (fields[key]['en-GB'].sys) {
+                    if (fields[key]['en-GB'].sys.linkType === 'Asset') {
+                      // promises.push(environment.getAsset(fields[key]['en-GB'].sys.id))
+                      return environment.getAsset(fields[key]['en-GB'].sys.id)
+                        .then(asset => {
+                          return asset.unpublish()
+                        })
+                        .then(asset => {
+                          return asset.delete()
+                        })
+                    } else {
+                      // promises.push(environment.getEntry(fields[key]['en-GB'].sys.id))
+                      return environment.getEntry(fields[key]['en-GB'].sys.id)
+                        .then(en => {
+                          return en.unpublish()
+                        })
+                        .then(en => {
+                          return en.delete()
+                        })
+                    }
+                  } 
+                })
+              })
+
+              // return Promise.all(promises)
             })
-          })
-
-          return Promise.all(promises)
-        })
-        .then(allEntries => {
-          // Lets unpublish ALL entries of Page
-          return allEntries.map(entry => {
-            const publishedVersion = entry.sys.publishedVersion
-            const version = entry.sys.version
-            // lets see if its published
-            if (publishedVersion && ((publishedVersion + 1) >= version)) {
-              // Now unpublish it
+            .then(() => {
               return entry.unpublish()
-            }
-            return entry
-          })
+            })
+            .then(e => {
+              return e.delete()
+            })
         })
-        .then(() => {
-          return entry.delete()
-        })
+        // .then(entries => {
+        //   // Move parent page to end of array so it gets deleted last
+        //   const pagesIndex = entries.findIndex(entry => {
+        //     return entry.sys.contentType.sys.id === 'pages'
+        //   })
+
+        //   entries.push(entries.splice(pagesIndex, 1)[0]);
+
+        //   return entries
+        // })
+        // .then(entries => {
+        //   // Unpublish and delete assets first
+        //   const promises = []
+        //   console.log("check for assets");
+
+        //   entries.map(entry => {
+
+        //     if (entry.sys.type === 'Asset') {
+        //       promises.push(entry.unpublish().then(() => entry.delete()))
+        //     } else {
+        //       promises.push(entry)
+        //     }
+        //   })
+
+        //   console.log('PROMISES', promises);
+
+        //   return Promise.all(promises)
+        // })
+        // .then(allEntries => {
+        //   console.log("ALL ENTRIES", allEntries);
+        //   const promises = []
+        //   // Lets unpublish ALL entries of Page
+        //   allEntries.map(entry => {
+        //     const publishedVersion = entry.sys.publishedVersion
+        //     const version = entry.sys.version
+        //     // lets see if its published
+        //     // console.log("ID", entry.sys.id, 'Type: ', entry.sys.type);
+        //     // console.log('published version: ', publishedVersion, 'version: ', version, '---', entry.fields.title['en-GB']);
+        //     if (publishedVersion && ((publishedVersion + 1) === version)) {
+        //       // Now unpublish it
+        //       console.log('unpublish');
+        //       promises.push(entry.unpublish())
+        //     } else {
+        //       console.log('dont unpublish', entry);
+        //       promises.push(entry)
+        //     }
+        //   })
+
+        //   console.log("promises", Promise.all(promises));
+        //   return Promise.all(promises)
+        // })
+        // .then(entries => {
+        //   const promises = [] 
+
+        //   entries.map(entry => {
+        //     console.log('DELETE ENTRY', entry);
+        //     promises.push(entry.delete())
+        //   })
+
+        //   return Promise.all(promises)
+        // })
+    })
+    .then(() => {
+      res.status(200).send({ message: 'Entries deleted' })
     })
     .catch(err => {
       res.status(500).send({ error: err })
