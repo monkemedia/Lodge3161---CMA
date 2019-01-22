2df2cd021895ad3d4696cbcb9e35f6f73f890bb6
monkemedia <info@monkemedia.co.uk>
2019-01-22 22:43:39 +0000

    Cleaned up code

--- api/controllers/content/deletePage.js
+++ api/controllers/content/deletePage.js
@@ -23,9 +23,6 @@ exports.deletePage = (req, res, next) => {
 
           return Promise.all(promises)
             .then(entries => {
-              // Now lets get Grand Child entries
-              // const promises = entries
-
               return entries.map(ent => {
                 const fields = ent.fields
 
@@ -53,77 +50,20 @@ exports.deletePage = (req, res, next) => {
                   } 
                 })
               })
-
-              // return Promise.all(promises)
             })
             .then(() => {
-              return entry.unpublish()
+              const publishedVersion = entry.sys.publishedVersion
+              const version = entry.sys.version
+
+              if (publishedVersion && ((publishedVersion + 1) === version)) {
+                return entry.unpublish()
+              }
+              return entry
             })
             .then(e => {
               return e.delete()
             })
         })
-        // .then(entries => {
-        //   // Move parent page to end of array so it gets deleted last
-        //   const pagesIndex = entries.findIndex(entry => {
-        //     return entry.sys.contentType.sys.id === 'pages'
-        //   })
-
-        //   entries.push(entries.splice(pagesIndex, 1)[0]);
-
-        //   return entries
-        // })
-        // .then(entries => {
-        //   // Unpublish and delete assets first
-        //   const promises = []
-        //   console.log("check for assets");
-
-        //   entries.map(entry => {
-
-        //     if (entry.sys.type === 'Asset') {
-        //       promises.push(entry.unpublish().then(() => entry.delete()))
-        //     } else {
-        //       promises.push(entry)
-        //     }
-        //   })
-
-        //   console.log('PROMISES', promises);
-
-        //   return Promise.all(promises)
-        // })
-        // .then(allEntries => {
-        //   console.log("ALL ENTRIES", allEntries);
-        //   const promises = []
-        //   // Lets unpublish ALL entries of Page
-        //   allEntries.map(entry => {
-        //     const publishedVersion = entry.sys.publishedVersion
-        //     const version = entry.sys.version
-        //     // lets see if its published
-        //     // console.log("ID", entry.sys.id, 'Type: ', entry.sys.type);
-        //     // console.log('published version: ', publishedVersion, 'version: ', version, '---', entry.fields.title['en-GB']);
-        //     if (publishedVersion && ((publishedVersion + 1) === version)) {
-        //       // Now unpublish it
-        //       console.log('unpublish');
-        //       promises.push(entry.unpublish())
-        //     } else {
-        //       console.log('dont unpublish', entry);
-        //       promises.push(entry)
-        //     }
-        //   })
-
-        //   console.log("promises", Promise.all(promises));
-        //   return Promise.all(promises)
-        // })
-        // .then(entries => {
-        //   const promises = [] 
-
-        //   entries.map(entry => {
-        //     console.log('DELETE ENTRY', entry);
-        //     promises.push(entry.delete())
-        //   })
-
-        //   return Promise.all(promises)
-        // })
     })
     .then(() => {
       res.status(200).send({ message: 'Entries deleted' })
